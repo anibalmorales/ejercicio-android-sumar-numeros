f45a91ae83d50f50ba6ff812ea26deb5344c302a
Luis Cabrera Benito <contacto@parzibyte.me>
2019-04-10 23:59:13 -0500

    Initial commit

---
+++ app/src/main/java/me/parzibyte/sumarnmeros/Actividad2.java
@@ -0,0 +1,28 @@
+package me.parzibyte.sumarnmeros;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Actividad2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad2);
+
+        // Obtener los datos que nos pasaron. El 0 es por si no se nos pasara
+        // nada, ya que se necesita un valor por defecto (defaultValue)
+        Intent intent = getIntent();
+        int sumaQueNosPasaron = intent.getIntExtra("suma", 0);
+
+        // Obtener Widget de vista
+        TextView textView = findViewById(R.id.textView);
+
+        // Armar un mensaje
+        String mensaje = "La suma es: " + sumaQueNosPasaron;
+        // Y ponerle la suma
+        textView.setText(mensaje);
+    }
+}
